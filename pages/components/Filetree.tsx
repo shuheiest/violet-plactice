b0faa22f3e8160d76e371d58713f38b9fda0e94f
shuhei.ueki <kamarukul211@docomo.ne.jp>
2021-09-05 15:43:38 +0900

    add:ファイルとフォルダを追加するボタンを作成

--- pages/components/Filetree.tsx
+++ pages/components/Filetree.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 
 const FiletreeArea = styled.div`
@@ -7,11 +8,52 @@ const FiletreeArea = styled.div`
   width: 200px;
   height: 100%;
   margin-right: auto;
-  background-color: #bab8bdaa;
+  background-color: #757377;
   border-color: rgb(150, 180, 190);
   border-width: 1px;
 `
 
+const AddFolder = styled.div`
+  position: absolute;
+  top: 90%;
+  left: 15%;
+  width: 130px;
+  height: 20px;
+  background-color: #e2e4cf;
+
+  a {
+    display: flex;
+    justify-content: center;
+    margin: auto;
+    text-align: center;
+  }
+`
+
+const AddFile = styled.div`
+  position: absolute;
+  top: 95%;
+  left: 15%;
+  width: 130px;
+  height: 20px;
+  background-color: #e2e4cf;
+
+  a {
+    display: flex;
+    justify-content: center;
+    margin: auto;
+    text-align: center;
+  }
+`
+
 export const FileTreeArea = () => {
-  return <FiletreeArea />
+  return (
+    <FiletreeArea>
+      <AddFolder>
+        <a>フォルダを追加</a>
+      </AddFolder>
+      <AddFile>
+        <a>ファイルを追加</a>
+      </AddFile>
+    </FiletreeArea>
+  )
 }
