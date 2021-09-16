498ec3ddab437d23676aef97260bb0a749e5d66d
shuhei.ueki <kamarukul211@docomo.ne.jp>
2021-09-16 22:02:02 +0900

    fix:子ファイルツリー処理を修正中

--- pages/components/ChildFileTree.tsx
+++ pages/components/ChildFileTree.tsx
@@ -16,7 +16,7 @@ export const ChildFileTreeArea: React.FC<useChildProps> = (parentFolder) => {
   const { api, onErr } = useApi()
   const [childfoldername, serChildfoldername] = useState('')
   const [foldername, setFoldername] = useState('')
-  const [childfolderNum, setchildfolderNum] = useState(0)
+  const [childFolderId, setChildFolderId] = useState(0)
   const { data: childfolder, error, mutate } = useAspidaSWR(api.childfolder)
   const inputChildFoldername = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => serChildfoldername(e.target.value),
@@ -30,46 +30,70 @@ export const ChildFileTreeArea: React.FC<useChildProps> = (parentFolder) => {
         .post({ body: { foldername: childfoldername, folderid: parentFolder.id } })
         .catch(onErr)
       if (!res) return
-      console.log('check')
-      registerParentFolder()
+      registerParentFolder(childfoldername)
       mutate()
     },
     [childfoldername]
   )
-  const registerParentFolder = useCallback(async () => {
-    if (!childfoldername) return
-    const res = await api.parentfolder
-      .post({ body: { foldername: childfoldername, rootflg: false } })
-      .catch(onErr)
-    if (!res) return
-    res.body.id
-    setchildfolderNum(res.body.id)
-    serChildfoldername('')
-    mutate()
-  }, [foldername])
+  const createChildFolderUnder = useCallback(
+    async (id: number) => {
+      if (!childfoldername) return
+      const res = await api.childfolder
+        .post({ body: { foldername: childfoldername, folderid: id } })
+        .catch(onErr)
+      if (!res) return
+      registerParentFolder(childfoldername)
+      mutate()
+    },
+    [childfoldername]
+  )
+
+  const registerParentFolder = useCallback(
+    async (foldername: string) => {
+      if (!childfoldername) return
+      const res = await api.parentfolder
+        .post({ body: { foldername: foldername, rootflg: false } })
+        .catch(onErr)
+      if (!res) return
+      setChildFolderId(res.body.id)
+      serChildfoldername('')
+      mutate()
+    },
+    [foldername]
+  )
+
   if (!childfolder) return <Fetching error={error} />
+
   return (
-    <ChildtreeArea>
-      {childfolder.map((childparentfolder) => (
-        <React.Fragment key={childparentfolder.id}>
-          {childparentfolder.folderid === parentFolder.id && (
-            <div>
-              <div>∟{childparentfolder.foldername}</div>
-              <form style={{ textAlign: 'center' }} onSubmit={createChildFolder}>
-                <input value={childfoldername} type="text" onChange={inputChildFoldername} />
-                <input type="submit" value="フォルダを追加" />
-              </form>
-            </div>
-          )}
-        </React.Fragment>
-      ))}
-      {/* <form style={{ textAlign: 'center' }} onSubmit={createChildFolder}>
+    <div>
+      <ChildtreeArea>
+        {childfolder.map((childparentfolder) => (
+          <React.Fragment key={childparentfolder.id}>
+            {childparentfolder.folderid === parentFolder.id && (
+              <div>
+                <div>∟{childparentfolder.foldername}</div>
+                {childfolder.find((e) => e.folderid === childFolderId) ? (
+                  <ChildFileTreeArea id={childFolderId} />
+                ) : null}
+                <form
+                  style={{ textAlign: 'center' }}
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    createChildFolderUnder(childFolderId)
+                  }}
+                >
+                  <input value={childfoldername} type="text" onChange={inputChildFoldername} />
+                  <input type="submit" value="孫フォルダを追加" />
+                </form>
+              </div>
+            )}
+          </React.Fragment>
+        ))}
+      </ChildtreeArea>
+      <form style={{ textAlign: 'center' }} onSubmit={createChildFolder}>
         <input value={childfoldername} type="text" onChange={inputChildFoldername} />
         <input type="submit" value="フォルダを追加" />
-      </form> */}
-      {childfolder.find((e) => e.folderid === childfolderNum) ? (
-        <ChildFileTreeArea id={childfolderNum} />
-      ) : null}
-    </ChildtreeArea>
+      </form>
+    </div>
   )
 }
